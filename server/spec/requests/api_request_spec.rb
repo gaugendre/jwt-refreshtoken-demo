fd1f147401445706f30aa0ad1bf1fcf7986bd36a
Guilhem Augendre <guilhem.augendre@gmail.com>
2021-02-10 15:32:36 +0100

    fix basic api request

--- server/spec/requests/api_request_spec.rb
+++ server/spec/requests/api_request_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-require_relative './user_jwt_requests_shared.rb'
-require_relative './json_api_shared.rb'
+require_relative './user_jwt_requests_shared'
+require_relative './json_api_shared'
 
 RSpec.describe 'Api', type: :request do
-  include_context "jwt on user request"
-  include_context "json api"
+  include_context 'jwt on user request'
+  include_context 'json api'
 
   let(:auth_header) do
     { 'Authorization' => "Bearer #{access_token}" }
@@ -16,12 +16,16 @@ RSpec.describe 'Api', type: :request do
       before { post api_path, headers: accept_header }
 
       it_behaves_like 'unauthorized response'
+      it_behaves_like 'no auth headers'
+      it_behaves_like 'error description'
     end
 
     context 'with an access_token' do
       before { post api_path, headers: accept_header.merge(auth_header) }
 
       it_behaves_like 'ok response'
+      
+      expect(json_content['user']).to be_present
     end
   end
 end
